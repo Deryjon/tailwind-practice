af461cd66feed160a9859e92fa996a54b2f7006c
lonewolf0844 <lonewolf.6120844@gmail.com>
2023-05-15 17:00:03 +0500

    add section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				"white": "#FFFFFF",
+				"brand": "#EE8313",
+			}
+			
+		},
+		container: {
+      center: true  
+    }
   },
   plugins: [],
 }
